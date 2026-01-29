3558b33d2a4ebfe208094ce02c252ffa811b286b
luvkushgithub <123541004+luvkushgithub@users.noreply.github.com>
2026-01-29 16:53:16 +0530

    Update hellofile.py

--- hellofile.py
+++ hellofile.py
@@ -4,6 +4,6 @@ import  socketserver
 PORT = 8000 
 
 Handler = http.server.SimpleHTTPRequestHandler
-with socketserver.TCPServer(("0.0.0.0",PORT)),Handler) as https:
+with socketserver.TCPServer(("0.0.0.0",PORT)),Handler) as httpd:
     print(f"Serving at port {PORT}")
     httpd.serve_forever()
